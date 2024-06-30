f78297f3010d8ccdb3997c5166d58d1647c35aaf
y0ub <y0lit7515@gmail.com>
2024-06-30 12:08:57 +0900

    update: comment and function name.

--- database/dbms/IDatabaseRepository.h
+++ database/dbms/IDatabaseRepository.h
@@ -1,20 +1,23 @@
 #ifndef Y0_CCUTIL_DATABASE_DBMS_IDATAREPOSITORY_H
 #define Y0_CCUTIL_DATABASE_DBMS_IDATAREPOSITORY_H
 
-#include <vector>
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 
 namespace y0_ccutil {
+  /**
+   * @brief interface for dbms with repository pattern
+   */
   class IDataRepository {
     public:
       virtual ~IDataRepository() = default;
 
-      virtual void createTable(const std::string& tableName, const std::vector<std::pair<std::string, std::string>>& columns) = 0;
-      virtual void insertRecord(const std::string& tableName, const std::map<std::string, std::string>& data) = 0;
-      virtual std::vector<std::map<std::string, std::string>> getRecords(const std::string& tableName) = 0;
-      virtual void updateRecord(const std::string& tableName, int id, const std::map<std::string, std::string>& data) = 0;
-      virtual void deleteRecord(const std::string& tableName, int id) = 0;
+      virtual void CreateTable(const std::string& tableName, const std::vector<std::pair<std::string, std::string>>& columns) = 0;
+      virtual void InsertRecord(const std::string& tableName, const std::map<std::string, std::string>& data) = 0;
+      virtual std::vector<std::map<std::string, std::string>> FetchRecords(const std::string& tableName) = 0;
+      virtual void UpdateRecord(const std::string& tableName, int id, const std::map<std::string, std::string>& data) = 0;
+      virtual void DeleteRecord(const std::string& tableName, int id) = 0;
   };
 }  // namespace y0_ccutil
 
